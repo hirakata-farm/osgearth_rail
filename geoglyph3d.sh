1e23c312d93145b15b20bfde525abdb4d6d66ec4
Yuki Osada <osada@neptune.fritz.box>
2025-08-30 22:41:33 +0900

    add start stop args

--- geoglyph3d.sh
+++ geoglyph3d.sh
@@ -1,17 +1,45 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/YOURPATH/OSG/dist/lib
-
 export DISPLAY=:0
 
-build/osgearth_rail earth/osmrail.earth
+# for shared memory , defined ghRail.hpp
+GFILE=/tmp/geoglyph3d
+
+if [ $# -lt 1 ]; then
+  echo "$0 [ start | stop | pid ]"
+  exit 1
+fi
+
+case "$1" in
+  start)
+      if [ -e ${GFILE} ]; then
+	  rm ${GFILE}
+      fi
+      build/osgearth_rail earth/osmrail.earth &
+      echo $! > ${GFILE}
+      ;;
+  stop)
+      PID=`cat ${GFILE}`
+      kill -KILL ${PID}
+      if [ -e ${GFILE} ]; then
+	  rm ${GFILE}
+      fi
+      ;;
+  pid)
+      if [ -e ${GFILE} ]; then
+	  cat ${GFILE}
+      else
+          echo "$0 not started"
+      fi
+
+      ;;
+  *)
+      echo "$0 [ start | stop | pid ]"
+      exit 1
+esac
+exit 0
 
 #gdb --args build/osgearth_rail  earth/osmrail.earth
-##set follow-fork-mode child
-
-
-
-
-
-
+#set follow-fork-mode child
 
