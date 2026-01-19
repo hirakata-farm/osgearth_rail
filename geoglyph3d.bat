4930cb8da832415064436a5e2731fb5625f131ae
Yuki Osada <osada@neptune.fritz.box>
2026-01-19 22:25:43 +0900

    linux and windows startup script

---
+++ geoglyph3d.bat
@@ -0,0 +1,15 @@
+REM geoglyph bat file for Windows
+
+REM set PATH=%PATH%;D:\OSG\vcpkg\installed\x64-windows\bin
+REM set PATH=%PATH%;D:\OSG\vcpkg\installed\x64-windows\plugins
+REM set PATH=%PATH%;D:\OSG\vcpkg\installed\x64-windows\tool
+REM set PATH=%PATH%;D:\OSG\vcpkg\installed\x64-windows\lib
+REM set PATH=%PATH%;D:\OSG\rail\build\Debug
+set PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\dotnet\;C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\;C:\Program Files\CMake\bin;C:\Program Files\Git\cmd;C:\Users\ootor\AppData\Local\Programs\Python\Launcher\;C:\Users\ootor\AppData\Local\Microsoft\WindowsApps;C:\Users\ootor\.dotnet\tools;C:\Users\ootor\AppData\Local\Python\Python314;D:\OSG\vcpkg\installed\x64-windows\bin;D:\OSG\vcpkg\installed\x64-windows\lib;D:\OSG\dist\bin;D:\OSG\dist\bin\osgPlugins-3.7.0
+
+set FONTCONFIG_PATH=D:\OSG\vcpkg\installed\x64-windows\etc\fonts
+
+build\Debug\osgearth_rail.exe earth\osmrail.earth
+
+
+
